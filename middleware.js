76bfedfe1bc61d77016f11b877380d327c0ee758
Charlotte Tennant <charlottetennant@Charlotte-Ts-MacBook-Air-2.local>
2023-04-16 10:39:06 +0100

    validation updated

--- middleware.js
+++ middleware.js
@@ -3,16 +3,6 @@ const express = require("express");
 module.exports = function (app) {
   app.use(express.static("public"));
 
-  app.use(express.json()); //parses the json
+  app.use(express.json()); 
 
-  // const dogs = [];
-
-  // app.get("/api/v1/dogs", (req, res) => {
-  //   res.status(200).json(dogs);
-  // });
-
-  // app.post("/api/v1/dogs", (req, res) => {
-  //   dogs.push(req.body);
-  //   res.sendStatus(201);
-  // });
 };
--- routes/dogs.routes.js
+++ routes/dogs.routes.js
@@ -1,6 +1,6 @@
 const path = require("path");
 const express = require("express");
-const { body, check } = require("express-validator");
+const { body } = require("express-validator");
 //get hold of Router from express
 const router = express.Router();
 
@@ -11,20 +11,81 @@ const {
   removeDog,
 } = require("../controllers/dog.controller");
 
+const MIN_NAME_LEN = 1;
+const MAX_NAME_LEN = 20;
+
+const MIN_BREED_LEN = 1;
+const MAX_BREED_LEN = 50;
+
+const MIN_PLAYFULNESS = 0;
+const MAX_PLAYFULNESS = 10;
+
 router
   .get("/:id?", getDogs)
   .post(
     "/",
-    body("name").not().isEmpty().trim().escape(),
-    body("breed").not().isEmpty().trim().escape(),
-    check("playfulness").matches(/\d/).withMessage("Must be a number!"),
+    body("name")
+      .not()
+      .isEmpty()
+      .withMessage("Value for 'name' is required.")
+      .trim()
+      .escape()
+      .isString()
+      .withMessage("Name must be a string.")
+      .isLength({ min: MIN_NAME_LEN, max: MAX_NAME_LEN })
+      .withMessage(
+        `Value for 'name' must be between ${MIN_NAME_LEN} and ${MAX_NAME_LEN} characters long.`,
+      ),
+    body("breed")
+      .not()
+      .isEmpty()
+      .withMessage("Value for 'breed' is required.")
+      .trim()
+      .escape()
+      .isString()
+      .withMessage("Breed must be a string.")
+      .isLength(
+        `Value for 'breed' must be between ${MIN_BREED_LEN} and ${MAX_BREED_LEN} characters long.`,
+      ),
+    body("playfulness")
+      .not()
+      .isEmpty()
+      .withMessage("A value for playfulness is required.")
+      .isInt({ min: MIN_PLAYFULNESS, max: MAX_PLAYFULNESS })
+      .withMessage(
+        `Value for 'playfulness' must be positive integer between ${MIN_PLAYFULNESS} and ${MAX_PLAYFULNESS}.`,
+      ),
     addDog,
   )
   .put(
     "/:id",
-    body("name").not().isEmpty().trim().escape(),
-    body("breed").not().isEmpty().trim().escape(),
-    check("playfulness").matches(/\d/).withMessage("Must be a number!"),
+    body("name")
+      .optional()
+      .trim()
+      .escape()
+      .isString()
+      .withMessage("Name must be a string.")
+      .isLength({ min: MIN_NAME_LEN, max: MAX_NAME_LEN })
+      .withMessage(
+        `Value for 'name' must be between ${MIN_NAME_LEN} and ${MAX_NAME_LEN} characters long.`,
+      ),
+    body("breed")
+      .optional()
+      .withMessage("Value for 'breed' is required.")
+      .trim()
+      .escape()
+      .isString()
+      .withMessage("Breed must be a string.")
+      .isLength(
+        `Value for 'breed' must be between ${MIN_BREED_LEN} and ${MAX_BREED_LEN} characters long.`,
+      ),
+    body("playfulness")
+      .optional()
+      .withMessage("A value for playfulness is required.")
+      .isInt({ min: MIN_PLAYFULNESS, max: MAX_PLAYFULNESS })
+      .withMessage(
+        `Value for 'playfulness' must be positive integer between ${MIN_PLAYFULNESS} and ${MAX_PLAYFULNESS}.`,
+      ),
     updateDog,
   )
   .delete("/:id", removeDog);
