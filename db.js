86b5feeff89920b7d6b8d43fe7cada67732c0be9
Charlotte Tennant <charlottetennant@Charlotte-Ts-MacBook-Air-2.local>
2023-04-16 10:24:36 +0100

    console logs removed

--- db.js
+++ db.js
@@ -2,8 +2,6 @@ const mongoose = require("mongoose");
 
 const { DB_URL = "mongodb://127.0.0.1:27017/test" } = process.env;
 
-console.log("DB_URL", DB_URL);
-
 async function main() {
   try {
     await mongoose.connect(DB_URL);
